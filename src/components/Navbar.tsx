1a55e6d05b4dfae5916b4ca4d2e40b73be9bf9d1
Jose Mendoza <developermendoza@gmail.com>
2024-06-09 12:26:19 -0700

    fix tab index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,6 @@ const Navbar = () => {
         {isModalOpen && (
           <div
             id="default-modal"
-            tabIndex="-1"
             aria-hidden="true"
             className="fixed inset-0 z-40 flex justify-center items-center w-full h-screen bg-gray-800 bg-opacity-50"
             onClick={toggleModal}
@@ -102,21 +101,6 @@ const Navbar = () => {
                     </div>
                   </form>
                 </div>
-                {/* Modal footer */}
-                {/* <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                  <button
-                    onClick={toggleModal}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    I accept
-                  </button>
-                  <button
-                    onClick={() => setIsModalOpen(false)}
-                    className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  >
-                    Decline
-                  </button>
-                </div> */}
               </div>
             </div>
           </div>
